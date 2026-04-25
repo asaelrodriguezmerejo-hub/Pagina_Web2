9ca4b201ef9034cf5a9daf613abef0a392da0682
asaelrodriguezmerejo-hub <asaelrodriguezmerejo@gmail.com>
2026-04-25 01:17:06 -0400

    Delete Practica directory

--- Practica/scriptA.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('.btn-pagina');
-button.addEventListener('click', () => {
-    window.location.href = '../index.html';
-});
-
-const contactButton = document.querySelector('.btn-redes');
-contactButton.addEventListener('click', () => {
-    window.location.href = 'https://www.instagram.com/asa_03l/';
-});
