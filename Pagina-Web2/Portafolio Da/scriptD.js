6d6d7c2172b034d1a58c32eef7c6b54de5f6d49a
asaelrodriguezmerejo-hub <asaelrodriguezmerejo@gmail.com>
2026-04-25 00:59:10 -0400

    Delete Pagina-Web2 directory

--- Pagina-Web2/Portafolio Da/scriptD.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector('button');
-
-button.addEventListener('click', () => {
-    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-});
--- Pagina-Web2/Portafolio-Digital-main/scriptE.js
+++
@@ -1,4 +0,0 @@
-const botton = document.querySelector('.btn-pagina');
-botton.addEventListener('click', () => {
-    window.location.href = '../index.html';
-});
