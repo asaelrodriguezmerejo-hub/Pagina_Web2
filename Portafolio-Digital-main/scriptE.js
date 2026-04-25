e2a15f23fb7261bdba84e5511a186648365aabf6
asaelrodriguezmerejo-hub <asaelrodriguezmerejo@gmail.com>
2026-04-25 01:00:13 -0400

    Add files via upload

---
+++ Portafolio-Digital-main/scriptE.js
@@ -0,0 +1,4 @@
+const botton = document.querySelector('.btn-pagina');
+botton.addEventListener('click', () => {
+    window.location.href = '../index.html';
+});
---
+++ Practica/scriptA.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.btn-pagina');
+button.addEventListener('click', () => {
+    window.location.href = '../index.html';
+});
+
+const contactButton = document.querySelector('.btn-redes');
+contactButton.addEventListener('click', () => {
+    window.location.href = 'https://www.instagram.com/asa_03l/';
+});
